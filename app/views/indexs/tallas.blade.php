fabc4de75cd872c4659d483e6da5b0f1b1e63fc4
Carlos <bakuryuthem0@gmail.com>
2015-06-17 17:05:43 -0430

    in the way to change images

---
+++ app/views/indexs/tallas.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.default')
+
+@section('content')
+<div class="hidden-container container-in">
+  <div class="titulo"><h1>¿COMO SABER LA TALLA DE SU TRAJE DE BAÑO?</h1></div>
+  <div class="contenido">
+  	<h4 style="text-align:center;">Si no conoces tu talla, y no sabes como elegirla, a continuacion te ofrecemos una pequeña guia de como encontrarla.</h4>
+  	<br>
+    <p class="textoPromedio"><strong>PECHO</strong>: Comenzar midiendo desde un lado manteniendo la cinta horizontal. Pase la cinta alrededor del cuerpo sujétela por la parte más ancha del pecho. </p>
+ 
+	<p class="textoPromedio"><strong>TORAX</strong>: Comience a medir desde un lado manteniendo la cinta horizontal. Pase la cinta alrededor del cuerpo, por debajo de los brazos y justo por debajo del pecho.</p>
+ 
+	<p class="textoPromedio"><strong>CINTURA</strong>: Mídala por la parte más estrecha, manteniendo la cinta horizontal.</p>
+ 
+	<p class="textoPromedio"><strong>TORSO</strong>: De una vuelta completa a la cinta desde un punto del hombro situado entre la base del cuello y la articulación del hombro, pasando por la parte más ancha del pecho; descienda hasta el punto central de la entrepierna y suba por la espalda hasta el punto inicial.</p>
+	<p class="textoPromedio">Le recomendamos comenzar midiendo primero por la espalda y pasar la cinta por la entrepierna para obtener un resultado más fácil.</p>
+	<br>
+	<h4 style="text-align:center;">ELEGIR LA TALLA ADECUADA</h4>
+	<br>
+	<p class="textoPromedio">Si las medidas se encuentran a medio camino entre dos tallas, escoja la talla más pequeña si prefiere y un ajuste más ceñido; en caso de preferir un ajuste holgado, escoja la talla más grande.</p>
+	<p class="textoPromedio">Si las medidas de cadera y cintura sugirieran dos tallas diferentes, elija la recomendada en función a la cadera. </p>
+ 	
+	<h4 style="text-align:center;" data-toggle="collapse" data-target=".tallasCollapse" class="toCollapse"><i class="fa fa-hand-o-right dedo"></i> Click aqui para ver las <strong>Medidas</strong></h4>
+  	<div class="collapse tallasCollapse">
+		<p class="textoPromedio">TALLA “S” = MEDIDA 36</p>
+		<ul class="textoPromedio">
+			<li>PECHO = 85 cm / 88 cm</li>
+			<li>TORAX = 69 cm / 71 cm</li>
+			<li>CINTURA = 65 cm / 67 cm</li>
+			<li>CADERA = 90 cm / 93 cm</li>
+			<li>TORSO = 149 cm / 152 cm</li>
+		</ul>
+	 
+		<p class="textoPromedio">TALLA “M” = MEDIDA 38</p>
+		<ul class="textoPromedio">
+			<li>PECHO = 90 cm / 93 cm</li>
+			<li>TORAX = 74 cm / 76 cm</li>
+			<li>CINTURA = 70 cm / 72 cm</li>
+			<li>CADERA = 95 cm / 98 cm</li>
+			<li>TORSO = 156 cm / 160 cm</li>
+		</ul>
+	 
+		<p class="textoPromedio">TALLA “L” = MEDIDA 40</p>
+		<ul class="textoPromedio">
+			<li>PECHO = 97 cm / 100 cm</li>
+			<li>TORAX = 80 cm / 84 cm</li>
+			<li>CINTURA = 76 cm / 80 cm</li>
+			<li>CADERA = 102 cm / 105 cm</li>
+			<li>TORSO = 164 cm / 168 cm</li>
+		</ul>
+	  
+	 	<p class="textoPromedio">TALLA “XL”= MEDIDA 42</p>
+		<ul class="textoPromedio">
+			<li>PECHO = 104 cm</li>
+			<li>TORAX = 88 cm</li>
+			<li>CINTURA = 84 cm</li>
+			<li>CADERA = 109 cm</li>
+			<li>TORSO = 171 cm</li>
+		</ul>
+	</div>
+
+
+  </div>
+</div>
+
+@stop
